3ac0f6e1ef244c08411b215e2190c144df50f6f7
marrgarett <sb6560259101@lru.ac.th>
2023-11-15 11:37:30 +0700

    first commit

---
+++ ch3/login.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync') ()
+let pwdcheck = parseFloat(prompt ('password : '))
+correctpwd = 1234
+var pwdresult
+if (pwdcheck != realpwd) {
+    pwdresult = ("password ไม่ถูกต้อง")
+} else {
+    pwdresult = ("password ถูกต้อง")
+}
+console.log(pwdresult)
---
+++ test.js
@@ -0,0 +1,51 @@
+// console.log(12345)
+// console.log("Hello World")
+/* console.log('One','Two','Three')
+console.log('สังสรรค์ หล้าพันธ์')
+*/
+
+/*
+var x, y, z
+var result
+let message
+*/
+
+/*
+var x = 123
+x = 4.56
+var y = 7.11
+y = 101
+
+console.log(x)
+console.log(y)
+*/
+
+/*
+var d = "JavaScript"
+var s = 'JavaScript'
+var b = `JavaScript`
+*/
+
+/*
+const PI = 3.141
+PI = 3.14
+console.log(PI)
+*/
+
+/*
+var a = 10/2
+var b = 100/6
+let c = 10/2.5
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+/*
+var a = 2 ** 3
+var b = 10 % 5
+let c = 13 & 4
+
+console.log(a, b, c)
+*/
